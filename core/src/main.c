76089f32e1408fd416890b6634c8ec5dc819dceb
Andreas PEDERSEN <andreas.pedersen@st.com>
2025-05-20 10:32:54 +0200

    add different demos

--- core/src/main.c
+++ core/src/main.c
@@ -21,6 +21,20 @@ static void SystemClock_Config(void);
 static void CPU_CACHE_Enable(void);
 static void MX_GPIO_Init(void);
 
+void demo_screen_blit(void);
+void demo_screen_blend(void);
+void demo_screen_text(void);
+
+lv_obj_t * ui_Menu;
+static lv_obj_t * my_rect;
+static lv_obj_t * my_other_rect;
+lv_obj_t * ui_Set_Minutes1;
+
+const char *loremIpsumText = 
+    "Donec pretium massa sapien, vel ultricies mauris malesuada ut. Sed id magna consequat, dictum turpis maximus, interdum neque. Integer tristique ex ac nulla tincidunt, id pellentesque massa imperdiet. Suspendisse convallis in metus ut egestas. Praesent magna elit, lacinia id turpis eu, fermentum accumsan leo. Mauris urna nibh, convallis vel augue sit amet, pretium dignissim elit. Quisque ornare sapien porttitor ex aliquet porta. Sed et lacus sit amet eros gravida pellentesque ut quis urna. Suspendisse aliquet nisl mauris, vitae consequat diam hendrerit id. Phasellus ligula urna, venenatis vitae neque sit amet, placerat faucibus ipsum. Integer eu felis sed quam mattis luctus. Vestibulum sed risus sit amet enim accumsan imperdiet quis quis neque. Donec accumsan aliquet sagittis. Pellentesque condimentum lobortis efficitur. Quisque sollicitudin tristique elit, euismod congue erat convallis ullamcorper. Etiam ultricies venenatis justo, at tincidunt turpis vulputate eu. Maecenas sodales condimentum tempus. Duis vitae ultricies ligula. Aliquam erat volutpat. Quisque porttitor nibh et urna placerat, quis congue sapien gravida. Vestibulum eget turpis at justo porttitor luctus a sed lectus. Aenean semper ante quis nunc efficitur, non varius tortor pulvinar. Morbi purus diam, condimentum id felis sit amet, commodo vehicula dui. Ut ut risus vitae nibh vehicula aliquam vitae eget nisi. Praesent vel tellus eget lacus luctus vulputate. Donec eget dapibus ex. Nam felis orci, gravida in velit et, gravida sodales lacus. Duis non scelerisque erat. Nullam fermentum vestibulum feugiat. Etiam egestas a nulla ut porta. Donec et lobortis massa, vel laoreet quam. Integer finibus nulla ut nisi dapibus, imperdiet suscipit dui scelerisque. Maecenas turpis neque, semper a molestie eget, semper convallis nibh. Nulla porttitor justo sed efficitur blandit. Ut commodo nulla nulla, nec bibendum purus gravida ut. Vivamus ultricies eget massa a porta. Sed consequat, nulla placerat fringilla placerat, est nisl rutrum erat, ut molestie risus enim ut ipsum.";
+
+#define DEMO 3
+
 int main(void) {
 
 	/* Enable the CPU Cache */
@@ -47,7 +61,15 @@ int main(void) {
 	tft_init();
 	touchpad_init();
 
+#if DEMO == 0
 	lv_demo_widgets();
+#elif DEMO == 1
+    demo_screen_blit();
+#elif DEMO == 2
+    demo_screen_blend();
+#elif DEMO == 3
+    demo_screen_text();
+#endif
 
 	while(1) {
         HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
@@ -56,6 +78,55 @@ int main(void) {
         lv_obj_invalidate(lv_scr_act());
 		HAL_Delay(100);
 	}
+}
+
+void demo_screen_blit()
+{
+    ui_Menu = lv_obj_create(NULL);
+
+    my_rect = lv_obj_create(ui_Menu);
+    lv_obj_set_size(my_rect, 800, 480);
+    lv_obj_set_pos(my_rect, 0, 0);
+    lv_obj_set_style_bg_color(my_rect, (lv_color_t)LV_COLOR_MAKE(0, 0, 255), 0);
+
+    lv_disp_load_scr(ui_Menu);
+}
+
+void demo_screen_blend()
+{
+    ui_Menu = lv_obj_create(NULL);
+
+    my_rect = lv_obj_create(ui_Menu);
+    lv_obj_set_size(my_rect, 800, 480);
+    lv_obj_set_pos(my_rect, 0, 0);
+    lv_obj_set_style_bg_color(my_rect, (lv_color_t)LV_COLOR_MAKE(0, 0, 255), 0);
+
+    my_other_rect = lv_obj_create(ui_Menu);
+    lv_obj_set_size(my_other_rect, 800, 480);
+    lv_obj_set_pos(my_other_rect, 0, 0);
+    lv_obj_set_style_bg_color(my_other_rect, (lv_color_t)LV_COLOR_MAKE(255, 0, 0), 0);
+    lv_obj_set_style_bg_opa(my_other_rect, LV_OPA_50, 0);
+
+    lv_disp_load_scr(ui_Menu);
+}
+
+void demo_screen_text()
+{
+    ui_Menu = lv_obj_create(NULL);
+
+    my_rect = lv_obj_create(ui_Menu);
+    lv_obj_set_size(my_rect, 800, 480);
+    lv_obj_set_pos(my_rect, 0, 0);
+    lv_obj_set_style_bg_color(my_rect, (lv_color_t)LV_COLOR_MAKE(0, 0, 255), 0);
+
+    ui_Set_Minutes1 = lv_label_create(ui_Menu);
+    lv_obj_set_width(ui_Set_Minutes1, lv_pct(80));   /// 1
+    lv_obj_set_height(ui_Set_Minutes1, LV_SIZE_CONTENT);    /// 1
+    lv_label_set_text(ui_Set_Minutes1, loremIpsumText);
+    lv_obj_set_style_text_color(ui_Set_Minutes1, lv_color_hex(0xFFFFFF), LV_PART_MAIN | LV_STATE_DEFAULT);
+    lv_obj_set_style_text_opa(ui_Set_Minutes1, 255, LV_PART_MAIN | LV_STATE_DEFAULT);
+
+    lv_disp_load_scr(ui_Menu);
 }
 
 void GPIO_1_SET(int state)

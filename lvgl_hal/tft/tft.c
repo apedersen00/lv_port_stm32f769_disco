f77b6a65412b875055a001efc4bf7e48cf58f8ce
Andreas PEDERSEN <andreas.pedersen@st.com>
2025-05-20 11:37:45 +0200

    align framebuffers

--- lvgl_hal/tft/tft.c
+++ lvgl_hal/tft/tft.c
@@ -144,8 +144,8 @@ void tft_init(void)
 
 	DMA_Config();
 
-	static uint8_t buf1[TFT_HOR_RES * 48 * 2];
-	static uint8_t buf2[TFT_HOR_RES * 48 * 2];
+	static uint8_t buf1[TFT_HOR_RES * 48 * 2] __attribute__((aligned(32)));
+	static uint8_t buf2[TFT_HOR_RES * 48 * 2] __attribute__((aligned(32)));
 	disp = lv_display_create(800, 480);
 	lv_display_set_buffers(disp, buf1, buf2, TFT_HOR_RES * 48 * 2, LV_DISP_RENDER_MODE_PARTIAL);
 	lv_display_set_flush_cb(disp, tft_flush_cb);
